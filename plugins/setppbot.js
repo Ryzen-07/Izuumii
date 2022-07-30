ffa7e6876899ebf22eb90468d2fd5d054cdfc100
Ryzzeenn <110073369+Ryzzeenn@users.noreply.github.com>
2022-07-30 15:26:03 +0700

    Create setppbot.js

---
+++ plugins/setppbot.js
@@ -0,0 +1,16 @@
+let handler = async (m, { conn, args }) => {
+    let bot = conn.user.jid // Bot
+    let q = m.quoted ? m.quoted : m
+    let mime = (q.msg || q).mimetype || ''
+    if (/image/.test(mime)) {
+      let img = await q.download()
+      if (!img) throw `Foto tidak ditemukan`
+     conn.updateProfilePicture (bot, img)
+    conn.reply(m.chat, 'Sukses Mengganti Foto Profile Bot!', m)
+	}
+    }
+handler.help = ['setppbot']
+handler.command = /^(setppbot)$/i
+handler.owner = true
+
+export default handler
