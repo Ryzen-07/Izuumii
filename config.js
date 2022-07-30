ad3c9939a7cc9de476b7a9f9cdff471d2b20b72e
Ryzzeenn <110073369+Ryzzeenn@users.noreply.github.com>
2022-07-30 14:39:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -48,7 +48,7 @@ global.flaaa = [
 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text=',
 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=',
 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text=']
-global.thumbloc = ['https://telegra.ph/file/67cfcbc73d3a68ac5d799.jpg', 'https://telegra.ph/file/67cfcbc73d3a68ac5d799.jpg']
+global.thumbloc = ['https://upload-xfar05.herokuapp.com/file/53dO7Onntqma.jpg', 'https://upload-xfar05.herokuapp.com/file/53dO7Onntqma.jpg']
 
 //=============『 Api-key 』============== //
 global.APIs = { // API Prefix
